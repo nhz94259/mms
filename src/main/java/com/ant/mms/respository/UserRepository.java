b8a32a0ddb908fb719dafbb0f7a3f57693012e0a
nhz94259@163.com <nhz94259@163.com>
2018-12-27 00:57:36 +0800

    团队用户接口开发

--- src/main/java/com/ant/mms/respository/UserRepository.java
+++ src/main/java/com/ant/mms/respository/UserRepository.java
@@ -5,16 +5,22 @@ package com.ant.mms.respository;
 import com.ant.mms.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by wolf
  */
 public interface UserRepository extends JpaRepository<User,String> {
 
-    User findUserByUsernameAndPassword(String username, String md5Password);
+    Optional<User> findUserByUsernameAndPassword(String username, String md5Password);
 
     List<User> findUserByUsername(String username);
 
     List<User> findUserByEmail(String email);
+
+    List<User> findUsersByLeaderIdIn(List<String> leaderList);
+
+    Optional<User> findByInviteCode(String code);
 }
